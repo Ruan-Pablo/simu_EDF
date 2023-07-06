1c030b45e95dc9fb01b486dec9a35f386322a3cf
Ruan Pablo <ruestacio@gmail.com>
2023-07-05 23:29:11 -0300

    de thread para tarefa

--- EDF.py
+++ EDF.py
@@ -2,34 +2,34 @@
 import time
 ###
 #  coisas necessárias para esse EDF 
-#  array de cada thread vai possui[periodo, custo, deadline relativa, proximo_periodo, quanto de custo ainda falta, deadline_absoluto]
-#  Funcionamento: o "escalonador" ira escolher no inicio de cada tempo, a thread com a deadline mais proxima e
-#  tentar executar ela até seu fim ou até o inicio do periodo de outra thread na qual a deadline esteja proxima, 
-#  caso a thread termine todo seu custo, a deadline absoluta aumenta a quantidade da relativa e aguarda o inicio do proximo periodo para poder reiniciar o custo.
+#  array de cada tarefa vai possui[periodo, custo, deadline relativa, proximo_periodo, quanto de custo ainda falta, deadline_absoluto]
+#  Funcionamento: o "escalonador" ira escolher no inicio de cada tempo, a tarefa com a deadline mais proxima e
+#  tentar executar ela até seu fim ou até o inicio do periodo de outra tarefa na qual a deadline esteja proxima, 
+#  caso a tarefa termine todo seu custo, a deadline absoluta aumenta a quantidade da relativa e aguarda o inicio do proximo periodo para poder reiniciar o custo.
 #  caso nenhum periodo tenha começado, o processador ficara INATIVO  ###
 
-def EDF(threads:list[dict]):
+def EDF(tarefas:list[dict]):
     img = ['','','']
-    mmc = MMC(threads) #reinicio de ciclos
+    mmc = MMC(tarefas) #reinicio de ciclos
     intax = 0.0
     print(f'tempo de reinicio de ciclo: {mmc}')
 
     tempo_atual = 0
     while(tempo_atual!= mmc):
         print(f'tempo atual:{tempo_atual}')
-        thread = encontrar_thread(threads)
-        if(thread!=None):
-            if(thread['deadline_absoluta']<tempo_atual):
-                print(f'Tarefa{thread["nome"]} passou da deadline')
+        tarefa = encontrar_tarefa(tarefas)
+        if(tarefa!=None):
+            if(tarefa['deadline_absoluta']<tempo_atual):
+                print(f'Tarefa{tarefa["nome"]} passou da deadline')
                 break
             else:
-                executar(thread)
-                if(thread["nome"] == 1):
+                executar(tarefa)
+                if(tarefa["nome"] == 1):
                     img[0]+=("◼ ")
                     img[1]+=("◻ ")
                     img[2]+=("◻ ")
 
-                elif(thread["nome"] == 2):
+                elif(tarefa["nome"] == 2):
                     img[0]+=("◻ ")
                     img[1]+=("◼ ")
                     img[2]+=("◻ ")
@@ -50,7 +50,7 @@ def EDF(threads:list[dict]):
 
 
         #checa se é inicio de algum periodo
-        atualizar_custos(threads,tempo_atual)
+        atualizar_custos(tarefas,tempo_atual)
 
    
 
@@ -62,10 +62,10 @@ def EDF(threads:list[dict]):
 
 
 
-def MMC(threads:list):
+def MMC(tarefas:list):
     valores = []
-    for thread in threads:
-        valores.append(thread['periodo'])
+    for tarefa in tarefas:
+        valores.append(tarefa['periodo'])
 
 
     maxin = max(valores)
@@ -82,31 +82,31 @@ def MMC(threads:list):
     return mmc
     
 
-def encontrar_thread(threads:list[dict]):
-    thread_encontrada = None
-    for thread in threads:
-        if(thread['custo_restante'] !=0 ):
-            if(thread_encontrada == None or thread['deadline_absoluta'] < thread_encontrada['deadline_absoluta'] or (thread['deadline_absoluta'] == thread_encontrada['deadline_absoluta'] and thread['custo_restante']<thread['custo'])):    
-                 thread_encontrada = thread
+def encontrar_tarefa(tarefas:list[dict]):
+    tarefa_encontrada = None
+    for tarefa in tarefas:
+        if(tarefa['custo_restante'] !=0 ):
+            if(tarefa_encontrada == None or tarefa['deadline_absoluta'] < tarefa_encontrada['deadline_absoluta'] or (tarefa['deadline_absoluta'] == tarefa_encontrada['deadline_absoluta'] and tarefa['custo_restante']<tarefa['custo'])):    
+                 tarefa_encontrada = tarefa
 
 
-    return thread_encontrada
+    return tarefa_encontrada
 
-def atualizar_custos(threads:list[dict],tempo_atual:int):
-    for thread in threads:
-        if(thread['prox_periodo'] == tempo_atual and thread['custo_restante']==0): 
-            thread['custo_restante'] = thread['custo']
-            thread['prox_periodo'] += thread['periodo'] 
+def atualizar_custos(tarefas:list[dict],tempo_atual:int):
+    for tarefa in tarefas:
+        if(tarefa['prox_periodo'] == tempo_atual and tarefa['custo_restante']==0): 
+            tarefa['custo_restante'] = tarefa['custo']
+            tarefa['prox_periodo'] += tarefa['periodo'] 
 
-def executar(thread:dict):
+def executar(tarefa:dict):
     
 
 
-    thread['custo_restante'] -= 1
-    print(f'executando tarefa {thread["nome"]} custo restante : {thread["custo_restante"]} / {thread["custo"]}')
+    tarefa['custo_restante'] -= 1
+    print(f'executando tarefa {tarefa["nome"]} custo restante : {tarefa["custo_restante"]} / {tarefa["custo"]}')
 
-    if(thread['custo_restante'] == 0):
-        thread['deadline_absoluta'] += thread['deadline_relativa']
+    if(tarefa['custo_restante'] == 0):
+        tarefa['deadline_absoluta'] += tarefa['deadline_relativa']
 
     #time.sleep(0.25)
 
@@ -117,24 +117,24 @@ def executar(thread:dict):
 
 a = open('Parte1-SistemasTestes\sistema3.txt','r')
 modes = a.readline().strip('\n').split('\t')
-threads = []
+tarefas = []
 i = 1 
 for line in a:
-    threads.append([i]+line.strip('\n').split('\t'))
+    tarefas.append([i]+line.strip('\n').split('\t'))
     i+=1
     
-print(threads,modes)
+print(tarefas,modes)
 
 in_dic = []
-for thread in threads:
+for tarefa in tarefas:
     label = {
-        'nome':int(thread[0]),
-        'periodo':int(thread[1]),
-        'custo':int(thread[2]),
-        'deadline_relativa':int(thread[3]),
-        'prox_periodo':int(thread[1]),
-        'custo_restante':int(thread[2]),
-        'deadline_absoluta':int(thread[3])
+        'nome':int(tarefa[0]),
+        'periodo':int(tarefa[1]),
+        'custo':int(tarefa[2]),
+        'deadline_relativa':int(tarefa[3]),
+        'prox_periodo':int(tarefa[1]),
+        'custo_restante':int(tarefa[2]),
+        'deadline_absoluta':int(tarefa[3])
     }
     in_dic.append(label)
 
